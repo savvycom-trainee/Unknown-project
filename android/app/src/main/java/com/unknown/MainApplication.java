c3b2088ceedddfe6b38b24a820e96d9798a4477b
trungducng <caoboiyb1997@gmail.com>
2018-05-17 15:29:18 +0700

    Add react-native-firebase + react-naitve-maps

--- android/app/src/main/java/com/unknown/MainApplication.java
+++ android/app/src/main/java/com/unknown/MainApplication.java
@@ -3,10 +3,12 @@ package com.unknown;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +24,12 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseAuthPackage(),
+          new RNFirebaseDatabasePackage(),
+          new RNFirebaseStoragePackage(),
+          new MapsPackage()
       );
     }
 
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,21 @@
 import React, { PureComponent } from 'react';
 import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
 
 class Home extends PureComponent {
   state = {};
   render() {
     return (
-      <View>
-        <Text>Hello World!!!</Text>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
     );
   }
