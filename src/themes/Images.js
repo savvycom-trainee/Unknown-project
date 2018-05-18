bdcc1912438f362ac2bd00b973b789ff4f87e167
piggy2303 <nguyentuananh23031997@gmail.com>
2018-05-18 20:54:05 +0700

    move image from imgs to new images

--- src/themes/Images.js
+++ src/themes/Images.js
@@ -2,10 +2,10 @@
 /* eslint-disable */
 const images = {
   restaurantPhoto: require('../../assets/Images/restaurantPhoto.png'),
-  restaurantPhotoMenu1: require('../../assets/imgs/restaurantPhotoMenu1.png'),
-  restaurantPhotoMenu2: require('../../assets/imgs/restaurantPhotoMenu2.png'),
-  restaurantPhotoMenu3: require('../../assets/imgs/restaurantPhotoMenu3.png'),
-  restaurantPhotoMenu4: require('../../assets/imgs/restaurantPhotoMenu4.png'),
+  restaurantPhotoMenu1: require('../../assets/Images/restaurantPhotoMenu1.png'),
+  restaurantPhotoMenu2: require('../../assets/Images/restaurantPhotoMenu2.png'),
+  restaurantPhotoMenu3: require('../../assets/Images/restaurantPhotoMenu3.png'),
+  restaurantPhotoMenu4: require('../../assets/Images/restaurantPhotoMenu4.png'),
 };
 
 export default images;
