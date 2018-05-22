3a3f6f982f615deec8d04976c05276d88ac4c1fc
chienvu123 <chienvu1308@gmail.com>
2018-05-22 09:05:46 +0700

    modal

--- src/navigators/authStack.js
+++ src/navigators/authStack.js
@@ -1,10 +1,14 @@
 import { createStackNavigator } from 'react-navigation';
 import Login from '../containers/Login';
+import Signup from '../containers/Signup';
 
 export default createStackNavigator(
   {
     Login: {
       screen: Login,
+    },
+    Signup: {
+      screen: Signup,
     },
   },
   {
