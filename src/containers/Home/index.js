e4b3473c0e04deb594c06af167eec9a1450ceb1e
hoanghaihuy <hoanghuy27297@gmail.com>
2018-06-13 13:40:06 +0700

    update UI

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, ScrollView, TouchableOpacity, Image, FlatList, Modal } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, Image, FlatList, Modal, RefreshControl } from 'react-native';
 import { connect } from 'react-redux';
 import Moment from 'moment';
 import firebase from 'react-native-firebase';
@@ -22,7 +22,7 @@ class Home extends PureComponent {
       longitude: null,
       modalVisible: false,
       error: null,
-      // starCount: 2.5,
+      refreshing: false,
     };
   }
 
@@ -52,7 +52,7 @@ class Home extends PureComponent {
         console.log(`position ${JSON.stringify(this.props.getPositionSuccess(position))}`);
         console.log(`state: ${JSON.stringify(this.state)}`);
       },
-      error => {
+      (error) => {
         this.setState({ error });
         this.props.getPositionFail();
       },
@@ -66,6 +66,7 @@ class Home extends PureComponent {
   hideModal = (message) => {
     this.setModalVisible(message);
   };
+
   _updateLocation = (lat, lng) => {
     const { uid } = this.props.user.user;
     console.log(this.props.user);
@@ -108,12 +109,6 @@ class Home extends PureComponent {
         <FlatList
           data={this.props.dataNewFeed.data.reverse()}
           renderItem={({ item }) => (
-            // const distance = this._getDistanceFromLatLonInKm(
-            //   item.location.lat,
-            //   item.location.lng,
-            //   this.state.latitude,
-            //   this.state.longitude,
-            // );
             <View style={styles.formItem}>
               <TouchableOpacity
                 onPress={() => {
